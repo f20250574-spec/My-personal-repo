f6c6d4b271578191b90bf3e51903f0ffe2fca80a
anaf-siddi <anafsiddiahmeda@gmail.com>
2025-09-29 11:20:30 +0400

    hi

--- cplab1/sample1.c
+++ cplab1/sample1.c
@@ -3,23 +3,15 @@
 void main() {
 
 int a,b,c,d,e;
-printf("enter subj 1 marks");
-scanf("%d", &a);
-printf("enter subj 2 marks");
-scanf("%d", &b);
-printf("enter subj 3 marks");
-scanf("%d", &c);
-printf("enter subj 4 marks");
-scanf("%d", &d);
-printf("enter subj 5 marks");
-scanf("%d", &e);
+printf("enter marks of subjects 1, 2 ,3, 4, 5:\n");
+scanf("%d\n %d\n %d\n %d\n %d", &a, &b, &c, &d, &e);
 
 int average= (a+b+c+d+e)/5;
 
-printf("The average marks obtained is %d", average);
+printf("The average marks obtained is %d\n", average);
 
 float percentage= (a+b+c+d+e)/5;
 
 
-printf("The aggregate percentage obtained is %f%%", percentage);
+printf("The aggregate percentage obtained is %.2f%%\n", percentage);
 }
