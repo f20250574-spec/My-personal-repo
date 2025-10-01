7b0a8bc9951a1b7c79167dcc09fc135abd8267df
anaf <anafsiddiahmeda@gmail.com>
2025-10-01 10:29:14 +0400

    hi

--- cplab3/ans3.c
+++ cplab3/ans3.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 
 void main() {
-int choice;
-printf("enter a digit 0-9");
-scanf("%d",&choice);
-switch (choice) {
-	case 0: printf("red");
-		break;
-	case '#': printf("blue");
-		break;
-	case '@': printf("yellow");
-		break;
-	case '$': printf("violet");
-		break;
-	case '*': printf("green");
-		break;
-	case '^': printf("indigo");
-		break;
-	case '&': printf("orange");
-		break;
 
-	default:
-		printf("invalid input");
-		break;
+int num;
+printf("enter a digit from 0-9");
+scanf("%d", &num);
+
+switch(num) {
+case 0: printf("Zero"); break;
+case 1: printf("One"); break;
+case 2: printf("Two"); break;
+case 3: printf("Three"); break;
+case 4: printf("Four"); break;
+case 5: printf("Five"); break;
+case 6: printf("Six"); break;
+case 7: printf("Seven"); break;
+case 8: printf("Eight"); break;
+case 9: printf("Nine"); break;
+case 10: printf("Ten"); break;
+default:printf("Enter a valid integer 0-9");
 }
+
 }
