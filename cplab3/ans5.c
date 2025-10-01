142eb856657042b213d29dd4315f9494565c11cb
anaf <anafsiddiahmeda@gmail.com>
2025-10-01 09:56:55 +0400

    hi

--- cplab3/ans5.c
+++ cplab3/ans5.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 
 void main() {
-char choice;
-printf("! for red\n@ for yellow\n# for blue\n$ for violet\n* for green\n^ for indigo\n& for orange\n");
-scanf("%c",&choice);
-switch (choice) {
-	case '!': printf("red");
-		break;
-	case '#': printf("blue");
-		break;
-	case '@': printf("yellow");
-		break;
-	case '$': printf("violet");
-		break;
-	case '*': printf("green");
-		break;
-	case '^': printf("indigo");
-		break;
-	case '&': printf("orange");
-		break;
-	default:
-		printf("invalid input");
-		break;
+
+char ch;
+printf("enter a character");
+scanf("%c", &ch);
+
+switch(ch) {
+	case 'V': 
+	case 'v':printf("Violet"); break;
+	case 'I':
+	case 'i':printf("Indigo"); break;
+	case 'B':
+	case 'b':printf("Blue"); break;
+	case 'G':
+	case 'g':printf("Green"); break;
+	case 'Y':
+	case 'y':printf("Yellow"); break;
+	case 'O':
+	case 'o':printf("Orange"); break;
+	case 'R':
+	case 'r':printf("Red"); break;
+	default:printf("Color is not in rainbow");
 }
+
 }
