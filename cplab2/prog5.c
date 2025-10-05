7fe70ce200cf917d75a27abb4c9157deb8d1d0e1
anaf-siddi <anafsiddiahmeda@gmail.com>
2025-10-05 23:02:10 +0400

    ye

--- cplab2/prog5.c
+++ cplab2/prog5.c
@@ -4,11 +4,14 @@ void main() {
 char mychar;
 printf("enter lowercase character bro\n");
 scanf("%c", &mychar);
-if (mychar=='a' || mychar=='e' || mychar=='i' || mychar=='o' || mychar=='u') {
+if (mychar=='a' || mychar=='e' || mychar=='i' || mychar=='o' || mychar=='u' /
+    mychar=='A' || mychar=='E' || mychar=='I' || mychar=='O' || mychar=='U') {
 printf("vowel\n");
 }
-else {
+else if ((int)mychar<=122 && (int)mychar>=65) {
 printf("consonant\n");
 }
+else 
+    printf("Ensure you entered an alphabet");
 
 }
