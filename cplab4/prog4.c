5685588c043b79e305a040c610ae59db35714965
anaf-siddi <anafsiddiahmeda@gmail.com>
2025-10-08 13:06:05 +0400

    yo

--- cplab4/prog4.c
+++ cplab4/prog4.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 void main() {
 
-int i,n,a,b;
+int i,n,a,b,t;
 printf("enter n: ");
 scanf("%d", &n);
+b=0;
 a=1;
+
 for (i=1; i<=n; i++) {
-b=a;
-a= a+b;
-printf("%d", b);
+
+    printf("%d\n", a);
+    t =a+b;
+    b=a;
+    a=t;
 }
 
 }
